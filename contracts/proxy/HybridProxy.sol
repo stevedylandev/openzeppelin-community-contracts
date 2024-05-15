f055494197cf24f868a488c571cf6fab9f7c1063
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-05-15 08:47:35 +0200

    add HybridProxy

---
+++ contracts/proxy/HybridProxy.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import {IBeacon} from "@openzeppelin/contracts/proxy/beacon/IBeacon.sol";
+import {ERC1967Utils} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Utils.sol";
+import {Proxy} from "@openzeppelin/contracts/proxy/Proxy.sol";
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+
+contract HybridProxy is Proxy {
+    constructor(address implementation, bytes memory data)
+    {
+        ERC1967Utils.upgradeToAndCall(implementation, "");
+        if (data.length > 0) {
+            Address.functionDelegateCall(_implementation(), data);
+        }
+    }
+
+    function _implementation()
+        internal
+        view
+        override
+        returns (address)
+    {
+        address implementation = ERC1967Utils.getImplementation();
+        try IBeacon(implementation).implementation() returns (address result) {
+            return result;
+        } catch {
+            return implementation;
+        }
+    }
+}
