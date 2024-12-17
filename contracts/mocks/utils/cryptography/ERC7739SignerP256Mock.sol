7ff222538fbc997013fd41940176759d694f3b8f
Ernesto García <ernestognw@gmail.com>
2024-12-17 15:23:53 -0600

    Make AccountSignerERC7702 draft and adjust mocks (#41)
    * Make AccountERC7702 draft and adjust mocks
    
    * Fix compilation

--- contracts/mocks/utils/cryptography/ERC7739SignerP256Mock.sol
+++ contracts/mocks/utils/cryptography/ERC7739SignerP256Mock.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.20;
 
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
-import {ERC7739Signer} from "../../../../utils/cryptography/draft-ERC7739Signer.sol";
-import {SignerP256} from "../../../../utils/cryptography/SignerP256.sol";
+import {ERC7739Signer} from "../../../utils/cryptography/draft-ERC7739Signer.sol";
+import {SignerP256} from "../../../utils/cryptography/SignerP256.sol";
 
 contract ERC7739SignerP256Mock is ERC7739Signer, SignerP256 {
     constructor(bytes32 qx, bytes32 qy) EIP712("ERC7739SignerP256", "1") {
--- contracts/mocks/utils/cryptography/ERC7739SignerRSAMock.sol
+++ contracts/mocks/utils/cryptography/ERC7739SignerRSAMock.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.20;
 
 import {EIP712} from "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
-import {ERC7739Signer} from "../../../../utils/cryptography/draft-ERC7739Signer.sol";
-import {SignerRSA} from "../../../../utils/cryptography/SignerRSA.sol";
+import {ERC7739Signer} from "../../../utils/cryptography/draft-ERC7739Signer.sol";
+import {SignerRSA} from "../../../utils/cryptography/SignerRSA.sol";
 
 contract ERC7739SignerRSAMock is ERC7739Signer, SignerRSA {
     constructor(bytes memory e, bytes memory n) EIP712("ERC7739SignerRSA", "1") {
