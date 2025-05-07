7eb97c0ef8d48e1a8a41d3ba6a4455eba21f8d87
Ernesto García <ernestognw@gmail.com>
2025-05-06 23:07:01 -0600

    Fix ERC7913SignatureVerifierZKEmail compilation (#144)

--- contracts/utils/cryptography/ERC7913SignatureVerifierZKEmail.sol
+++ contracts/utils/cryptography/ERC7913SignatureVerifierZKEmail.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.24;
 
 import {IDKIMRegistry} from "@zk-email/contracts/DKIMRegistry.sol";
-import {IVerifier} from "@zk-email/email-tx-builder/interfaces/IVerifier.sol";
-import {EmailAuthMsg} from "@zk-email/email-tx-builder/interfaces/IEmailTypes.sol";
+import {IVerifier} from "@zk-email/email-tx-builder/src/interfaces/IVerifier.sol";
+import {EmailAuthMsg} from "@zk-email/email-tx-builder/src/interfaces/IEmailTypes.sol";
 import {IERC7913SignatureVerifier} from "../../interfaces/IERC7913.sol";
 import {ZKEmailUtils} from "./ZKEmailUtils.sol";
 
