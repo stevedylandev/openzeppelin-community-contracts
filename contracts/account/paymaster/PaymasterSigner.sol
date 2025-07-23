8f26d9fbe873a721e19641b14f4e24cf71182f13
Ernesto García <ernestognw@gmail.com>
2025-07-23 09:37:50 -0600

    Update dependencies (#197)

--- contracts/account/paymaster/PaymasterSigner.sol
+++ contracts/account/paymaster/PaymasterSigner.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.24;
 
 import {ERC4337Utils, PackedUserOperation} from "@openzeppelin/contracts/account/utils/draft-ERC4337Utils.sol";
 import {AbstractSigner} from "@openzeppelin/contracts/utils/cryptography/signers/AbstractSigner.sol";
