b91151f25f5f4b3a5a2027f724d0731999049d67
Hadrien Croubois <hadrien.croubois@gmail.com>
2025-08-20 16:39:22 +0200

    Flatten AxelarGatewayAdapter contract (#202)

--- test/crosschain/axelar/AxelarHelper.js
+++ test/crosschain/axelar/AxelarHelper.js
@@ -5,8 +5,8 @@ async function deploy(owner) {
   const chain = await getLocalChain();
 
   const axelar = await ethers.deployContract('AxelarGatewayMock');
-  const gatewayA = await ethers.deployContract('AxelarGatewayDuplex', [axelar, owner]);
-  const gatewayB = await ethers.deployContract('AxelarGatewayDuplex', [axelar, owner]);
+  const gatewayA = await ethers.deployContract('AxelarGatewayAdapter', [axelar, owner]);
+  const gatewayB = await ethers.deployContract('AxelarGatewayAdapter', [axelar, owner]);
 
   await Promise.all([
     gatewayA.connect(owner).registerChainEquivalence(chain.erc7930, 'local'),
